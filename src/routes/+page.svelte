f21c44f149c7d976f02920e17ad70a71607f511e
hallvardd <hallvard.stemshaug@gmail.com>
2025-08-19 20:09:06 +0200

    add styling to under construction page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to my homepage!</h1>
-<p>It's under construction</p>
+<body>
+    <div id="box">
+        <h1>Welcome to my homepage!</h1>
+        <p>It's under construction</p>
+    </div>
+</body>
+
+<style>
+    body { 
+        background-color: #ffa;
+    }
+    #box {
+        position: absolute;
+        background-color: #333;
+        width: 30%;
+        border-radius: 20px;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+    h1{
+        color: gold;
+        font-family: "Comic Sans MS", cursive;
+        text-align: center;
+    }
+    p{
+        color: #f55;
+        font-family: "Comic Sans MS", cursive;
+        font-size: 2em;
+        text-align: center;
+    }
+</style>
+
